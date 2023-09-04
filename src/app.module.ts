be8d7cd633b1842201d9e4c300b5b5ebbe339cd7
luccameds <luccameds@gmail.com>
2023-09-03 22:57:47 -0300

    Create the Todo CRUD with MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGOOSE_URL),
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
